7c26d5f49b3358740dd63c7695bb692359e04f0d
Jason Dana <jason.dana@gmail.com>
2015-04-24 15:09:12 -0400

    Fix url path for yahoo query api Use urllib2 instead of urllib

--- yahoo_finance/yql.py
+++ yahoo_finance/yql.py
@@ -1,6 +1,6 @@
-import httplib, urllib, simplejson, logging
+import httplib, urllib2, simplejson, logging
 
-PUBLIC_API_URL = 'htp://query.yahooapis.com/v1/public/yql'
+PUBLIC_API_URL = 'http://query.yahooapis.com/v1/public/yql'
 DATATABLES_URL = 'store://datatables.org/alltableswithkeys'
 
 log = logging.getLogger(__name__)
@@ -11,7 +11,7 @@ class YQLQuery(object):
         self.connection = httplib.HTTPConnection('query.yahooapis.com')
 
     def execute(self, yql, token = None):
-        self.connection.request('GET', PUBLIC_API_URL + '?' + urllib.urlencode({'q': yql, 'format': 'json', 'env': DATATABLES_URL}))
+        self.connection.request('GET', PUBLIC_API_URL + '?' + urllib2.urlencode({'q': yql, 'format': 'json', 'env': DATATABLES_URL}, True))
         response = self.connection.getresponse().read()
         try:
             response = simplejson.loads(response)
