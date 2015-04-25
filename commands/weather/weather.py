19310037ab3d8ea864ffe7cd5446d8566c0111fc
Jason Dana <jason.dana@gmail.com>
2015-04-25 10:11:07 -0400

    Update weather command program flow Look for ipv4 and ipv6 address in user host ident string Find location and weather based upon ip

--- commands/weather/weather.py
+++ commands/weather/weather.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm.exc import NoResultFound
 from commands.weather import User, Location
 from plugin.command import Command
 
-import httplib, urllib, simplejson, logging, forecastio
+import httplib, urllib, simplejson, logging, forecastio, re
 
 log = logging.getLogger(__name__)
 
@@ -31,10 +31,8 @@ class Weather(Command):
         if args[0] == 'add':
             user = self.get_user(nick, userhost)
             if user:
-                self.update_location(user, args[1:])
-                return "[{nick}] Location updated.".format(nick=nick)
-            location = self.add_location(nick, userhost, args[1:])
-            return "[{nick}] Location added.".format(nick=nick)
+                return self.update_location(user, args[1:])
+            return self.add_location(nick, userhost, args[1:])
         user = self.get_user(args[0], None)
         if user:
             return self.get_weather_by_user(user)
@@ -42,6 +40,8 @@ class Weather(Command):
 
     def add_location(self, nick, userhost, location):
         location = ' '.join(location)
+        if not self.get_weather_by_location(location):
+            return "[{nick}] Not a valid location.".format(nick=nick)
         Session = sessionmaker(bind=self.engine)
         session = Session()
         user = User(nick=nick, userhost=userhost)
@@ -51,16 +51,19 @@ class Weather(Command):
         session.commit()
         session.refresh(location)
         session.close()
-        return location
+        return "[{nick}] Location added.".format(nick=nick)
 
     def update_location(self, user, new):
         new = ' '.join(new)
+        if not self.get_weather_by_location(new):
+            return "[{nick}] Not a valid location.".format(nick=user.nick)
         Session = sessionmaker(bind=self.engine)
         session = Session()
         location = session.query(Location).filter(Location.user.has(id=user.id)).one()
         location.location = new
         session.commit()
         session.close()
+        return "[{nick}] Location updated.".format(nick=user.nick)
 
     def get_weather_by_user(self, user):
         Session = sessionmaker(bind=self.engine)
@@ -69,6 +72,9 @@ class Weather(Command):
         session.refresh(location)
         session.close()
         return self.get_weather_by_location(location.location)
+
+    def get_forecast(self, lat, lng):
+        return forecastio.load_forecast(API_KEY, lat, lng, units='us')
 
     def get_weather_by_location(self, location):
         if not location:
@@ -83,10 +89,34 @@ class Weather(Command):
         connection.close()
         try:
             response = simplejson.loads(response)
+            if not response['status'] == 'OK':
+                return
             results = response['results'][0]
             latlng = results['geometry']['location']
             name = results['formatted_address']
-            forecast = forecastio.load_forecast(API_KEY, latlng['lat'], latlng['lng'], units='us')
+            forecast = self.get_forecast(latlng['lat'], latlng['lng'])
+            curr = forecast.currently()
+            return name + sep + curr.summary + sep + str(curr.temperature) + unicode(' ºF', "utf-8") 
+        except Exception as e:
+            log.exception(e)
+            log.info(response)
+
+    def get_weather_by_ip(self, ip):
+        if not ip:
+            return
+        sep = " \x0307|\x03 "
+        api = '/json'
+        connection = httplib.HTTPConnection('ip-api.com')
+        requesturl = '{api}/{ip}'.format(api=api, ip=ip)
+        connection.request('GET', requesturl)
+        response = connection.getresponse().read()
+        connection.close()
+        try:
+            response = simplejson.loads(response)
+            if response['status'] != 'success':
+                return
+            name = response['city'] + ', ' + response['region'] + ' ' + response['zip']
+            forecast = self.get_forecast(response['lat'], response['lon'])
             curr = forecast.currently()
             return name + sep + curr.summary + sep + str(curr.temperature) + unicode(' ºF', "utf-8")
         except Exception as e:
@@ -96,6 +126,12 @@ class Weather(Command):
     def get_weather(self, nick, userhost):
         location = self.get_location(nick, userhost)
         if not location:
+            ipv4 = re.findall(r'\b\d{1,3}[\.\-]\d{1,3}[\.\-]\d{1,3}[\.\-]\d{1,3}\b', userhost)
+            ipv6 = re.findall(r'(?:(?:[0-9A-Fa-f]{1,4}:){6}(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|::(?:[0-9A-Fa-f]{1,4}:){5}(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:[0-9A-Fa-f]{1,4})?::(?:[0-9A-Fa-f]{1,4}:){4}(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4})?::(?:[0-9A-Fa-f]{1,4}:){3}(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:(?:[0-9A-Fa-f]{1,4}:){,2}[0-9A-Fa-f]{1,4})?::(?:[0-9A-Fa-f]{1,4}:){2}(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:(?:[0-9A-Fa-f]{1,4}:){,3}[0-9A-Fa-f]{1,4})?::[0-9A-Fa-f]{1,4}:(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:(?:[0-9A-Fa-f]{1,4}:){,4}[0-9A-Fa-f]{1,4})?::(?:[0-9A-Fa-f]{1,4}:[0-9A-Fa-f]{1,4}|(?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))|(?:(?:[0-9A-Fa-f]{1,4}:){,5}[0-9A-Fa-f]{1,4})?::[0-9A-Fa-f]{1,4}|(?:(?:[0-9A-Fa-f]{1,4}:){,6}[0-9A-Fa-f]{1,4})?::)', userhost)
+            if ipv4:
+                return self.get_weather_by_ip(ipv4[0].replace('-', '.'))
+            if ipv6:
+                return self.get_weather_by_ip(ipv6[0])
             return
         return self.get_weather_by_location(location)
 
